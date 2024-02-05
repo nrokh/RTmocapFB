aabe6964f514d62bee033ff6853c310fe81483c6
Nataliya Rokhmanova <n.rokhmanova@gmail.com>
2024-02-05 16:12:52 +0100

    added heel strike and toe off logic plus FPA storing

---
+++ RealtimeGaitDetection/StreamingDataFPAEstimation.py
@@ -0,0 +1,142 @@
+from vicon_dssdk import ViconDataStream
+import argparse
+import sys
+import time
+import math
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import keyboard #TODO: make sure this is installed
+
+# create arg to host (Vicon Nexus)
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('host', nargs='?', help="Host name, in the format of server:port", default = "localhost:801")
+args = parser.parse_args()
+
+client = ViconDataStream.Client()
+
+try:
+    # Connect to Nexus (Nexus needs to be on and either Live or replaying previously collected data)
+    client.Connect( args.host)
+    print( '        Connected to Nexus')
+
+    # Enable the data type
+    client.EnableMarkerData()
+
+    # Report whether data type was enabled successfully:
+    print ( '        Markers enabled? ', client.IsMarkerDataEnabled() )
+
+
+    # start getting frames 
+    HasFrame = False
+    timeout = 50
+    while not HasFrame:
+        print( '.' )
+        try:
+            if client.GetFrame():
+                HasFrame = True
+            timeout=timeout-1
+            if timeout < 0:
+                print('Failed to get frame')
+                sys.exit()
+        except ViconDataStream.DataStreamException as e:
+            client.GetFrame()
+
+    # Set streaming mode to Server Push (lowest latency, but buffers could overfill, resulting in dropped frames)
+    client.SetStreamMode( ViconDataStream.Client.StreamMode.EServerPush)
+    print( '        Current frame rate: ', client.GetFrameRate() )
+
+    # Get the subject's name
+    subjectNames = client.GetSubjectNames()
+    print('        Subject name: ', subjectNames)
+
+    # create a list to store FPA and marker values
+    FPA_store = []
+    CAL_store = []
+    PSI_store = []
+    DIFFDV_store = [10,10,10] # TODO: check if this is how you want to initialize
+
+    while True: # wait for keyboard interrupt
+        subjectName = subjectNames[0] # select the main subject
+        client.GetFrame() # get the frame
+
+
+
+        ################# ENTER BASELINE FPA ###############
+        baselineFPA = float(input("Enter subject's baseline FPA and hit enter: "))
+
+        ################# CALCULATE FPA ####################
+
+        # calculate FPA (horizontal plane, so X and Y components only):
+
+        # TODO: include error exception for an occluded marker
+        footVec = (client.GetMarkerGlobalTranslation( subjectName, 'RTOE')[0][0]- client.GetMarkerGlobalTranslation( subjectName, 'RHEE')[0][0],
+                   client.GetMarkerGlobalTranslation( subjectName, 'RTOE')[0][1] - client.GetMarkerGlobalTranslation( subjectName, 'RHEE')[0][1])
+
+        FPA = -math.degrees(math.atan(footVec[1]/footVec[0])) # TODO: check signs for right foot
+
+
+
+
+
+        ################# STEP DETECTION ###################
+        print("Press space when ready to start step detection: ")
+        keyboard.wait('space')         # TODO: check if keyboard input works; experimenter waits for steady-state
+
+        # get AP CAL and PSI markers (TODO: should be 0th index -- X component-- but check)
+        CAL = client.GetMarkerGlobalTranslation( subjectName, 'RCAL')[0][0]
+        CAL_store.append(CAL)
+        PSI = client.GetMarkerGlobalTranslation( subjectName, 'RPSI')[0][0]
+        PSI_store.append(CAL)
+
+        # take derivative of difference between heel and hip:
+        DIFF = CAL - PSI
+        DIFFDV_store.append(DIFF)
+
+        # search for local max
+        if DIFFDV_store[-1]<=0 and DIFFDV_store[-2]>=0 and DIFFDV_store[-3]>=0 and DIFFDV_store[-4]>=0:
+
+            print("local max")
+            FPAstep_store = []
+            local_min_detected = False
+
+            while not local_min_detected:
+                FPAstep_store.append(FPA)
+
+                # search for local min
+                if DIFFDV_store[-1]>=0 and DIFFDV_store[-2]<=0 and DIFFDV_store[-3]<=0 and DIFFDV_store[-4]<=0:
+                    
+                    local_min_detected = True
+                    print("local min")
+                    # average FPA throughout stance
+                    meanFPAstep = np.mean(FPAstep_store)
+
+                    print("mean FPA for step = " + str(meanFPAstep))
+
+                    # TODO compare avg FPA to target
+
+                ################# CUE GAITGUIDE ###############
+                # SCALE FEEDBACK ACCORDING TO DISTANCE FROM TARGET
+
+        # save FPA value to the list
+        FPA_store.append(FPA)
+
+except KeyboardInterrupt: # CTRL-C to exit
+    # save calculated FPA
+    df = pd.DataFrame(FPA_store)
+    csv_file = 'H:\__CODE\Streaming\DSSDK_Scripts\FPA_Python.csv'
+    df.to_csv(csv_file)
+
+    # TODO: save HS/TO events with time stamps?
+    
+    # plot the FPA
+    plt.plot(FPA_store)
+    plt.xlabel('Frame')
+    plt.ylabel('FPA [deg]')
+    plt.show()
+
+
+except ViconDataStream.DataStreamException as e:
+    print( 'Handled data stream error: ', e )
+
+
