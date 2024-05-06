b4ad6cce2e3f4b6e0c84c6b30d4df386e371e36f
Vani Sundaram <vasu1360@colorado.edu>
2024-05-06 15:30:17 +0200

    fixed the setpairings

--- reset_pairings.m
+++ reset_pairings.m
@@ -7,14 +7,16 @@ setPairings3(:,3) = setPairings2(:,3);
 
 for i = 1:size(setPairings2,1)
     for j = 1:2
-        if setPairings2(i,j) == 240
+        if setPairings2(i,j) == 330
             setPairings3(i,j) = 300;
+        elseif setPairings2(i,j) == 240
+            setPairings3(i,j) = 270;
         elseif setPairings2(i,j) == 150
-            setPairings3(i,j) = 250;
+            setPairings3(i,j) = 220;
         elseif setPairings2(i,j) == 420
-            setPairings3(i,j) = 360;
+            setPairings3(i,j) = 330;
         elseif setPairings2(i,j) == 510
-            setPairings3(i,j) = 410;
+            setPairings3(i,j) = 380;
         else 
             setPairings3(i,j) = setPairings2(i,j);
         end
