609093d23c0e4806823f1d0f69e84bea91f855fd
Nataliya Rokhmanova <n.rokhmanova@gmail.com>
2024-07-31 16:11:53 -0500

    add features array

--- analysis/fullData_analysis/analyzeKAM_2024JUL31.py
+++ analysis/fullData_analysis/analyzeKAM_2024JUL31.py
@@ -5,22 +5,35 @@ import tkinter as tk
 from tkinter import filedialog
 import os
 
-#TODO: save FPAs from analyzeFPA file
+#TODO: save tFPAs from analyzeFPA file
 
 # 0. SETUP
 # a. set model and normalization coefficients from Rokhmanova et al. 2022
-b_weight = 0.5
-scale_weight = 0.0
-b_height = 0.5
-scale_height = 0.0
-b_speed = 0.5
-scale_speed = 0.0
-b_alignment = 0.5
-scale_alignment = 0.0
-b_bFPA = 0.5
-scale_bFPA = 0.0
-b_tFPA = 0.5
-scale_tFPA = 0.0
+offset = 0.35673
+
+b_weight = -0.01444
+mean_weight = 71.31296
+std_weight = 12.59157
+
+b_height = -0.00413
+mean_height = 172.37590
+std_height = 8.63829
+
+b_speed = 0.00340
+mean_speed = 1.12072
+std_speed = 0.00684
+
+b_alignment = -0.01475
+mean_alignment = 1.24025
+std_alignment = 3.16196
+
+b_bFPA = -0.00492
+mean_bFPA = 3.09735
+std_bFPA = 4.14900
+
+b_tFPA = 0.30261
+mean_tFPA = 5.5
+std_tFPA = 2.87
 
 # b. create arrays for storage
 subs_tot = 36
@@ -30,10 +43,12 @@ store_allrKAM_RT4 = np.zeros((subs_tot, 200))
 store_allrKAM_RET = np.zeros((subs_tot, 200))
 
 # 1. load feature array
-# TODO: create csv file containing walking speed, height, weight, and tFPA
 root = tk.Tk()
 root.withdraw() 
 directory = filedialog.askdirectory()
+
+inputFeatures_csv_file = os.path.normpath(os.path.join(directory, 'pKAM_features.csv'))
+inputFeatures = pd.read_csv(inputFeatures_csv_file)
 
 # 2. compute static alignment for each sub
 
