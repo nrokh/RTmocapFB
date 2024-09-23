baab17b38ff9a5b6ceda7618976fc204fd3077c9
Nataliya Rokhmanova <n.rokhmanova@gmail.com>
2024-09-23 08:09:55 -0500

    add rough draft for analyzing durations

---
+++ analysis/fullData_analysis/analyzeDurations_2024SEP20.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import filedialog
+import os
+
+# 1. set up file structure
+# 2. load a subject's tFPAs at RT1 and RT4
+# 3. compute durations
+# 4. save durations to two big arrays
+# 5. end loop; plot histograms 
